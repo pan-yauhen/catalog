80340adcc313112f68ef61458cd0d0afef57aa52
pan_evgene <evgeniy.panteleev@scorum.com>
2019-04-06 21:03:18 +0300

    add: header route-links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Header.scss';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 export const Header = () =>
-    <header className="header d-flex justify-content-between">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+    <header className="header d-flex justify-content-between align-items-center">
+        <div className="d-flex align-items-center">
+            <Link to={''}><img src={logo} alt='logo' /></Link>
+            <Link to={''} className="header-link">Весеннее поступление</Link>
+        </div>
+        <Link to={''} className="text-capitalize header-link mr-4">информация</Link>
     </header>;
