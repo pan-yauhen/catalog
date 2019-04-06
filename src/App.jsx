f105e8c16cc11d3874ffdd37676448e49a2e2e03
pan_evgene <evgeniy.panteleev@scorum.com>
2019-04-06 13:37:53 +0300

    create app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <header className="header">Hello</header>
+          <main className="main">World</main>
+          <footer className="footer">o/</footer>
+      </div>
+    );
+  }
+}
+
+export default App;
