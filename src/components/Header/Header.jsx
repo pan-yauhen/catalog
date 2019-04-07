9ceee6300b3f7caaadfec74880f4eee56e26cdc8
pan_evgene <evgeniy.panteleev@scorum.com>
2019-04-07 19:20:55 +0300

    add: adaptive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 
 export const Header = () =>
-    <header className="header d-flex justify-content-between align-items-center">
-        <div className="d-flex align-items-center">
+    <header className="header d-flex flex-column flex-sm-row justify-content-between align-items-center text-center">
+        <div className="d-flex flex-column flex-sm-row align-items-center">
             <Link to={''}><img src={logo} alt='logo' /></Link>
-            <Link to={''} className="header-link">Весеннее поступление</Link>
+            <Link to={''} className="my-3 my-sm-auto header-link">Весеннее поступление</Link>
         </div>
-        <Link to={''} className="text-capitalize header-link mr-4">информация</Link>
+        <Link to={''} className="my-3 my-sm-auto text-capitalize header-link mr-sm-4">информация</Link>
     </header>;
