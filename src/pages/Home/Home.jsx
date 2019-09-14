364f66f5a0796ae96a047fb854f775a9d03fd332
Chernovolod <johnstas977@gmail.com>
2019-09-14 15:23:57 +0300

    refactor: add Link props tu Button at Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import Button from 'reactstrap/es/Button'
 import './Home.scss';
 import HomeItems from '../../mocks/HomeItems';
 
@@ -13,13 +14,14 @@ export const Home = () =>
         <div className={`section-inner inner${i.id} d-flex flex-column justify-content-center h-100 px-3`}>
           <h3 className="font-weight-bold">{i.productName}</h3>
           <span>{i.description}</span>
-          <Link
-            to={'/catalog'}
+          <Button
+            tag={Link}
+            to={'catalog'}
             outline color={'info'}
             className="align-self-center text-white border-white rounded-0 w-75 mt-5"
           >
             ПОЛУЧИТЬ
-          </Link>
+          </Button>
         </div>
       </div>
     ))}
