f6e8cc360ba2b61cf949871fa11f18ef781a705e
Chernovolod <johnstas977@gmail.com>
2019-09-14 15:10:24 +0300

    feat: add links for catalog page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export const Header = () =>
     <header className="header d-flex flex-column flex-sm-row justify-content-between align-items-center text-center">
         <div className="d-flex flex-column flex-sm-row align-items-center">
             <Link to={''}><img src={logo} alt='logo' /></Link>
-            <Link to={''} className="my-3 my-sm-auto header-link">Весеннее поступление</Link>
+            <Link to={'/catalog'} className="my-3 my-sm-auto header-link">Весеннее поступление</Link>
         </div>
         <Link to={''} className="my-3 my-sm-auto text-capitalize header-link mr-sm-4">информация</Link>
     </header>;
