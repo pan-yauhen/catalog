f7cc9c8cc51f97b01820bb013e508ea6831c902c
Chernovolod <39561908+Chernovolod@users.noreply.github.com>
2019-09-15 17:16:31 +0300

    feat: add information page and map for it (#10)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,8 @@ import logo from '../../assets/logo.png';
 export const Header = () =>
     <header className="header d-flex flex-column flex-sm-row justify-content-between align-items-center text-center px-2">
         <div className="d-flex flex-column flex-sm-row align-items-center">
-            <Link to={''}><img src={logo} alt='logo' /></Link>
+            <Link to={'/'}><img src={logo} alt='logo' /></Link>
             <Link to={'/catalog'} className="my-3 my-sm-auto header-link">Весеннее поступление</Link>
         </div>
-        <Link to={''} className="my-3 my-sm-auto text-capitalize header-link mr-sm-4">информация</Link>
+        <Link to={'/information'} className="my-3 my-sm-auto text-capitalize header-link mr-sm-4">Информация</Link>
     </header>;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import './Main.scss';
-import { Home } from '../../pages/Home/Home';
-import { Catalog } from '../../pages/Catalog/Catalog';
+import {Home} from '../../pages/Home/Home';
+import {Catalog} from '../../pages/Catalog/Catalog';
+import {Information} from '../../pages/Information/Information'
 
 export const Main = () =>
-    <main className="main">
-        <Switch>
-            <Route exact
-                   path="/"
-                   component={Home} />
-            <Route exact
-                   path="/catalog"
-                   component={Catalog} />
-        </Switch>
-    </main>;
+  <main className="main">
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/catalog" component={Catalog}/>
+      <Route exact path="/information" component={Information}/>
+    </Switch>
+  </main>;
